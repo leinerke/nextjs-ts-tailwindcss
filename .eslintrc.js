bcb136c02e0dcf040f0fe979679ac96d7582c59f
Leinerke Suarez <leinerke2000@gmail.com>
2022-03-09 19:00:49 -0500

    🚀 Add eslint, tailwindcss, and typescript.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'next',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+  },
+};
