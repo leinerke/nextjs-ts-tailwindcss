196d14989366590626ee26523423af00e6299b3a
Leinerke Suarez <leinerke2000@gmail.com>
2022-06-23 20:22:03 -0500

    Config eslint and prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
   env: {
     browser: true,
-    amd: true,
+    es2021: true,
     node: true,
-    es6: true,
   },
   extends: [
-    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:jsx-a11y/recommended',
     'next',
     'next/core-web-vitals',
+    'prettier',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     semi: ['error', 'always'],
   },
